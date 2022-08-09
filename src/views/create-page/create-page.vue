965ebf43ae0932548dd358f288a8b63bdaad8185
yenche123 <yenche123@alumni.sjtu.edu.cn>
2022-08-09 15:18:10 +0800

    feat: 完成 创建房间的样式、布局和基础事件

--- src/views/create-page/create-page.vue
+++ src/views/create-page/create-page.vue
@@ -1,12 +1,113 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed, ref, onMounted } from 'vue';
 import { hasPreviousRouteInApp, goHome, useRouteAndPtRouter } from "../../routes/pt-router";
 import PtButton from "../../components/pt-button.vue"
+import cp from "./cp-helper"
 
+const { router, route } = useRouteAndPtRouter()
+const hasPrev = hasPreviousRouteInApp()
 const inputValue = ref<string>("")
+
+const canSubmit = computed(() => {
+  let val = inputValue.value
+  let v = val.trim()
+  if(v.length < 10) return false
+  const reg = /^http(s)?:\/\/[\w\.]*\w{1,32}\.\w{2,6}\S*$/g
+  return reg.test(val)
+})
+
+const onInputConfirm = () => {
+  if(!canSubmit.value) return
+  cp.finishInput(inputValue.value, router, route)
+}
+
+const onTapConfirm = () => {
+  if(!canSubmit.value) return
+  cp.finishInput(inputValue.value, router, route)
+}
+
+const onTapBack = () => {
+  if(hasPrev.value) router.go(-1)
+  else goHome(router)
+}
+
+// 让输入框在页面打开时聚焦
+const inputEl = ref<HTMLInputElement | null>(null)
+onMounted(() => {
+  if(canSubmit.value) return
+  inputEl.value?.focus()
+})
 
 </script>
 
 <template>
-  <h1>新建房间页</h1>
+  <div class="page">
+    <div class="page-container">
+      <h1>播客链接</h1>
+      <input 
+        v-model="inputValue" 
+        placeholder="请黏贴单集链接" 
+        type="url" 
+        @keyup.enter="onInputConfirm" 
+        maxlength="1000"
+        ref="inputEl"
+      />
+      <p>提示: 目前支持 xiaoyuzhoufm.com、podcasts.apple.com/cn/ 或者后缀为 .mp3 的链接</p>
+    </div>
+  </div>
+  <div class="page-btns-container">
+    <div class="page-btns">
+      <pt-button 
+        class="join-main-btn" 
+        text="确定" 
+        @click="onTapConfirm"
+        :disabled="!canSubmit"
+      />
+      <pt-button :text="hasPrev ? '返回' : '回首页'" type="other" @click="onTapBack"></pt-button>
+    </div>
+  </div>
 </template>
+
+<style scoped lang="scss">
+
+.page-container {
+
+  h1 {
+    margin-block-start: 0;
+    font-size: 38px;
+    line-height: 50px;
+    color: var(--text-color);
+    letter-spacing: 2px;
+    margin-bottom: 50px;
+  }
+
+  input {
+    font-size: 32px;
+    line-height: 46px;
+    color: var(--desc-color);
+    border: 0;
+    outline: none;
+    text-align: center;
+  }
+
+  input::-webkit-input-placeholder {
+    color: var(--note-color);
+  }
+
+  p {
+    margin-block-start: 30px;
+    margin-block-end: 30px;
+    font-size: 14px;
+    color: var(--note-color);
+    text-align: center;
+    max-width: 320px;
+    user-select: text;
+  }
+
+}
+
+.join-main-btn {
+  margin-bottom: 20px;
+}
+
+</style>
